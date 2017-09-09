8fd12554487692a20af0368234a99c670ab1e929
Sascha Häberling <mail@sascha-haeberling.com>
2017-09-09 14:28:12 -0700

    Add a 2-layer cache implementation.
    This one uses in-memory and memcache to cache data.

---
+++ appengine/src/main/java/org/retrostore/request/Cache.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2017, Sascha Häberling
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.retrostore.request;
+
+import com.google.common.base.Optional;
+
+/**
+ * A simple cache interface.
+ */
+public interface Cache {
+
+  /**
+   * Returns the data if it's in the cache, or otherwise calls the data provider for the data. If
+   * the latter happens, the data will then also be put into the cache.
+   *
+   * @param resourceName the name of the resource, used as a key into our cache.
+   * @param fallback     will be called to provide the data if it is not in the cache.
+   * @return The data.
+   */
+  Optional<byte[]> get(String resourceName, DataProvider fallback);
+
+  /**
+   * Classes implementing thia interface can provide data to fill in the cache if needed.
+   */
+  interface DataProvider {
+    /** Provide the data for the cache. */
+    byte[] provide();
+  }
+}
---
+++ appengine/src/main/java/org/retrostore/request/TwoLayerCacheImpl.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright 2017, Sascha Häberling
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.retrostore.request;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import org.retrostore.resources.MemcacheWrapper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * A two-level cache, using simple class-instance-local HashMap as the first level, and memcache
+ * as the second level.
+ */
+public class TwoLayerCacheImpl implements Cache {
+  private static final Logger LOG = Logger.getLogger("TwoLayerCache");
+
+  private static Map<String, byte[]> sFirstLevel = new HashMap<>(100);
+  private final MemcacheWrapper mMemcache;
+  private static int mNumServedFirstLevel = 0;
+  private static int mNumServedMemcache = 0;
+  private static int mNumCacheMisses = 0;
+
+  public TwoLayerCacheImpl(MemcacheWrapper memcache) {
+    mMemcache = memcache;
+  }
+
+  @Override
+  public Optional<byte[]> get(String resourceName, DataProvider fallback) {
+    Preconditions.checkNotNull(resourceName);
+    Preconditions.checkNotNull(fallback);
+
+    LOG.info("Current cache stats: " + toString());
+
+    if (sFirstLevel.containsKey(resourceName) && sFirstLevel.get(resourceName) != null) {
+      mNumServedFirstLevel++;
+      return Optional.of(sFirstLevel.get(resourceName));
+    }
+
+    Optional<byte[]> dataOpt = mMemcache.get(resourceName);
+    if (dataOpt.isPresent()) {
+      mNumServedMemcache++;
+      sFirstLevel.put(resourceName, dataOpt.get());
+      return dataOpt;
+    }
+
+    mNumCacheMisses++;
+    byte[] data = fallback.provide();
+    if (data != null && data.length > 0) {
+      sFirstLevel.put(resourceName, data);
+      mMemcache.put(resourceName, data);
+    }
+    return Optional.fromNullable(data);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Cache, served %d first-level and %d from memcache. Missed %d.",
+        mNumServedFirstLevel, mNumServedMemcache, mNumCacheMisses);
+  }
+}
