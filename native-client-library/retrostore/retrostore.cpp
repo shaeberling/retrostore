da7645d791952fbab27c6a5963d612a051a49e8c
Sascha Häberling <mail@sascha-haeberling.com>
2018-04-21 13:38:10 -0700

    Native client lib 0.0.4
    Can now decode a message successfully from an HTTP response.

--- native-client-library/retrostore/retrostore.cpp
+++ native-client-library/retrostore/retrostore.cpp
@@ -2,6 +2,7 @@
 
 #include "Arduino.h"
 #include <WiFi.h>
+#include <string>
 
 #include "rs_proto_api.pb.h"
 #include "pb_decode.h"
@@ -21,6 +22,33 @@ const string PATH_FETCH_MEDIA_IMAGES = "/api/fetchMediaImages";
 const string PATH_JSON_LIST_APPS = "rpc?m=pubapplist";
 }
 
+namespace {
+// Returns the length (in bytes) of the content in this HTTP response.
+int GetHttpResponseContentLength(uint8_t* http_response) {
+  // First we need to find the content length which we get in the header.
+  string buffer_str((const char*)http_response);
+  string content_length_key = "Content-Length: ";
+  int start_pos = buffer_str.find(content_length_key) + content_length_key.size();
+  if (start_pos < 0) {
+    Serial.println("Cannot find 'Content-Length:' marker.");
+    return -1;
+  }
+  int end_pos = buffer_str.find("\r\n", start_pos);
+  if (end_pos < 0) {
+    Serial.println("Cannot find end of content-lenth value.");
+    return -1;
+  }
+
+  if (start_pos >= end_pos) {
+    Serial.println("start >= end for content-length value range.");
+    return -1;
+  }
+  auto content_length_str = buffer_str.substr(start_pos, end_pos - start_pos);
+  return atoi(content_length_str.c_str());
+}
+
+}  // namespace
+
 RetroStore::RetroStore()
     : host_(DEFAULT_HOST), port_(DEFAULT_PORT) {
 }
@@ -30,7 +58,7 @@ RetroStore::RetroStore(const string& host, int port)
 }
 
 void RetroStore::PrintVersion() {
-  Serial.println("RetroStore v0.0.3");
+  Serial.println("RetroStore v0.0.4");
 }
 
 bool RetroStore::FetchApps(int start, int num) {
@@ -73,27 +101,35 @@ bool RetroStore::FetchUrl(const string& host_str,
   uint8_t buffer[BUFFER_SIZE];
   size_t message_length = client.read(buffer, BUFFER_SIZE);
   if (message_length >= BUFFER_SIZE) {
-    Serial.print("ERROR: Fetch buffer too small: " + String(BUFFER_SIZE));
+    Serial.println("ERROR: Fetch buffer too small: " + String(BUFFER_SIZE));
     return false;
   }
-  Serial.print("SUCCESS: Read" + String(message_length) + " bytes.");
+  Serial.println("SUCCESS: Read " + String(message_length) + " bytes.");
   Serial.println("Closing connection");
   client.stop();
 
+  // Extract content from HTTP response.
+  int content_length = GetHttpResponseContentLength(buffer);
+  uint8_t message[content_length];
+  for (int i = message_length - 1, j = content_length - 1; j >= 0; --i, --j) {
+    message[j] = buffer[i]; 
+  }
+
   // Decode the protocol buffer message.
   ApiResponseApps response = ApiResponseApps_init_zero;
-
-  // TODO: Extract response body from HTTP response.
-
-  pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
-  bool status = pb_decode_delimited(&stream, ApiResponseApps_fields, &response);
+  pb_istream_t stream = pb_istream_from_buffer(message, content_length);
+  bool status = pb_decode(&stream, ApiResponseApps_fields, &response);
 
   if (!status) {
     Serial.println("ERROR: Decoding of message failed: " + String(PB_GET_ERROR(&stream)));
-    Serial.println("Message as string: " + String((const char*)buffer));
     return false;
   }
   Serial.println("SUCCESS: Message decoded! :-)");
+
+  if (!response.success) {
+    Serial.println("Server reporter an ERROR");
+    return false;
+  }
 
   return true;
 }
