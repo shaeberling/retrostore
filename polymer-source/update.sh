5a12cc3da08c292c658b4c04158af31191777651
Sascha Häberling <mail@sascha-haeberling.com>
2019-01-12 16:00:05 -0800

    Update the update and run script to be more robust to errors.

--- polymer-source/update.sh
+++ polymer-source/update.sh
@@ -1,7 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
+SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Ensure that the Bower components have been installed.
+if [ ! -d "${SCRIPT_LOCATION}/bower_components" ]; then
+  echo "Bower components not present. Installing ..."
+  bower install
+else
+  echo "Bower components present."
+fi
+
+pushd "${SCRIPT_LOCATION}"
 polymer build --bundle --js-minify --css-minify --html-minify
+popd
 
-rm -r ../appengine/src/main/webapp/WEB-INF/polymer-app/*
-cp -R build/default/* ../appengine/src/main/webapp/WEB-INF/polymer-app/
+POLYMER_DEST="${SCRIPT_LOCATION}/../appengine/src/main/webapp/WEB-INF/polymer-app/"
+
+if [ -d "${POLYMER_DEST}" ]; then
+  echo "Deleting destination..."
+  rm -r "${POLYMER_DEST}"/*
+else
+  mkdir -p "${POLYMER_DEST}"
+fi
+cp -R "${SCRIPT_LOCATION}"/build/default/* "${POLYMER_DEST}"
 
